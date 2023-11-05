304b9ca8d00a8550b87afc93f28a53853bbaa056
Alex-Benites <136673432+Alex-Benites@users.noreply.github.com>
2023-11-04 20:17:13 -0500

    Eliminacion de Proyecto Anterior

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module proyecto.gestioncontacto {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens proyecto.gestioncontacto to javafx.fxml;
-    exports proyecto.gestioncontacto;
-}
